caab46c6876ac5abaa193be49038a6eced56fc0d
Harshita Surana <harshitasurana2005@gmail.com>
2026-04-14 15:20:09 +0530

    feat: add final animations and UI improvements

--- src/components/home/HomeBetweenText.jsx
+++ src/components/home/HomeBetweenText.jsx
@@ -5,7 +5,7 @@ const HomeBetweenText = () => {
         <div>
             <div className='font-[font2] pr-2'>
                 <div className='flex items-center justify-end '>
-                    <p className=' w-1/6'>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; K72 is an agency that builds brands from every angle. Today, tomorrow and years from now. We think the best sparks fly when comfort zones get left behind and friction infuses our strategies, brands and communications with real feeling. We’re transparent, honest and say what we mean, and when we believe in something, we’re all in.
+                    <p className=' w-1/5'>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; K72 is an agency that builds brands from every angle. Today, tomorrow and years from now. We think the best sparks fly when comfort zones get left behind and friction infuses our strategies, brands and communications with real feeling. We’re transparent, honest and say what we mean, and when we believe in something, we’re all in.
                     </p>
                 </div>
             </div>
